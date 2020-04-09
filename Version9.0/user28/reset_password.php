7ce89da8ec9924ab9a29e267f6c49fc7bfc5480d
238301 <60400694+238301@users.noreply.github.com>
2020-04-08 23:36:35 -0500

    final project

--- Version9.0/user28/reset_password.php
+++ Version9.0/user28/reset_password.php
@@ -74,22 +74,32 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 
 <head>
+    <link rel="shortcut icon" href="images/Favicon2.ico">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="SHS WebDev Login">
-
-    <title>Reset Password</title>
-<link rel="shortcut icon" href="">
-    <!-- Bootstrap core JS -->
-    <!-- These are needed to get the responsive menu to work -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="description" content="SHS WebDev Menu Sample">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>reset password</title>
+    <div>
+        <link href="CSS/SampleCSS.css" rel="stylesheet" type="text/css">
+        <link href="games2.html" rel="stylesheet" type="text/css">
+        <script src="JS/SampleJS.js"></script>
+        <!-- Bootstrap core JS -->
+        <!-- These are needed to get the responsive menu to work -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="JS/SampleJS.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="CSS/SampleCSS.css" rel="stylesheet" type="text/css">
+        <!-- Custom styles for this template -->
+        <style .button { background-color: #4CAF50; /* Green */ border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; }></style>
+    </div>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <style type="text/css">
         body {
             font: 14px sans-serif;
@@ -107,20 +117,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <body>
     <div class="menu">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand"><img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMgZJOU6m518_A443vctOUHCbiSFtcAJMJ8i4sVyZ8dW4-IvR6' height=50px width=50px></a>
+        <nav class="navbar navbar-expand-md navbar sticky-top navbar-dark bg-dark">
+            <a href="http://shakonet.isd720.com" class="navbar-brand">WebDev</a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <!---------------------------------- Edit These Items in your Menu ------------->
+                    <a href="MainIndex.html" class="nav-item nav-link active" onmouseover="style.color='red'" onmouseout="style.color='white'"> | Home |</a>
+                    <a href="AboutMe.html" class="nav-item nav-link active" onmouseover="style.color='red'" onmouseout="style.color='white'">| About Me |</a>
+                    <a href="DigitalDesign.html" class="nav-item nav-link active" tabindex="-2" onmouseover="style.color='red'" onmouseout="style.color='white'">| Digital Design |</a>
+                    <a href="Photography.html" class="nav-item nav-link active" tabindex="-2" onmouseover="style.color='red'" onmouseout="style.color='white'">| Photography |</a>
+                    <a href="games2.html" class="nav-item nav-link active" tabindex="-1" onmouseover="style.color='red'" onmouseout="style.color='white'">| Games |</a>
+                    <a href="VeryImportant.html" class="nav-item nav-link active" tabindex="-2" onmouseover="style.color='red'" onmouseout="style.color='white'">| Very Important |</a>
+                    <a href="Movies.html" class="nav-item nav-link active" tabindex="-2" onmouseover="style.color='red'" onmouseout="style.color='white'">| Movies |</a>
+                    <a href="mailto:jjszot@gmail.com?Subject=Hello" class="nav-item nav-link active" tabindex="-2" onmouseover="style.color='red'" onmouseout="style.color='white'">| Contact |</a>
+                    <!----------------------------------^ Edit These Items in your Menu ^ ------------->
+                </div>
                 <div class="navbar-nav ml-auto">
                     <a href="reset_password.php" class="nav-item nav-link active"><i class="fa fa-cog fa-lg" aria-hidden="true"></i><?php echo htmlspecialchars($_SESSION["username"]); ?></a>
 
                     <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
                     echo "<a href='logout.php' class='nav-item nav-link btn-danger' onclick='return confirm(\"Are you sure?\");'> Logout </a>";
                     } else { echo "<a href='login.php' class='nav-item nav-link'> Login </a>";} ?>
+
                 </div>
+
             </div>
         </nav>
     </div>
